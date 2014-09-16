f4a8a70a2706062846f41506418c5a685e07b18f
Adam Gray <z3329798@matht319.maths.unsw.edu.au>
2014-09-16 10:37:05 +1000

    Fixed issues with Project 1 Question 9. I have also spoken to the lecturer about issues arrising from the change of question 8. Note that Question_9.m must be run in matlab so the parallel code can run. If it can't be run in a modern version of matlab change the parfor to a for and the running will be slower but it will work.

--- Assignment_2/Project_1_Code/Question_9.m
+++ Assignment_2/Project_1_Code/Question_9.m
@@ -4,31 +4,32 @@ alpha           = 0.5;
 gamma           = 6 * pi;
 T               = @(theta, nu) [mod(theta + nu, 2*pi), alpha * nu + gamma*cos(theta + nu)];
 % First calculation of the expected return time
-initial         = [0.5, 0.5];
-N               = 1000000;
+initial         = [2, 2];
+N               = 100000;
 orbit           = zeros(N,2);
 orbit(1,:)      = initial;
-%for i=[2:N]
-%    orbit(i,:)  = T(orbit(i-1,1), orbit(i-1,2));
-%end
+for i=[2:N]
+    orbit(i,:)  = T(orbit(i-1,1), orbit(i-1,2));
+end
 mx              = max(orbit(:,2));
 high_count      = nnz(orbit(:,2) > 0.99 * mx);
 
-%exp_return_t_1  = N / high_count;
+exp_return_t_1  = N / high_count;
 
 %Second calculation of the expected return time
 N_b             = 1000;
-N               = 10000;
+N               = 100000;
 initials        = zeros(N_b, 2);
 k               = 1;
-for i = linspace(-10,10, 100);
+for i = linspace(-10,10, 100)
     for j               = linspace(0.5,1.5,10)
-        intials(k,:)    = [j,i];
-        k++;
+        initials(k,:)    = [j*pi,i*pi];
+        k               = k + 1;
     end
 end
+
 %Need a parallel for loop for this to run.
-%This *must* be run in a modern version of octave otherwise parfor must be changed.
+%This *must* be run in a modern version of Matlab otherwise parfor must be changed.
 return_times    = zeros(N_b,1);
 parfor i = 1:N_b
     orbit               = zeros(N,2);
@@ -37,15 +38,15 @@ parfor i = 1:N_b
         orbit(j,:)      = T(orbit(j-1,1), orbit(j-1,2));
     end
     mx                  = max(orbit(:,2));
-    highs               = find(nnz(orbit(:,2) > 0.99 * mx));
+    highs               = find(orbit(:,2) > 0.99 * mx);
     if (length(highs) >= 2)
         ret_time        = highs(2) - highs(1);
         return_times(i) = ret_time;
     end
-endparfor
+end
 
 %Only calculate average over sequences where there was a return time.
-return_times    = return_time(find(return_time));
-exp_return_t_2  = avg(return_times);
+return_times    = return_times(find(return_times));
+exp_return_t_2  = mean(return_times);
 
 
